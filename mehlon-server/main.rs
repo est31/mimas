bbdfe27d242cbe40a16e8a1de396c2c38195ad48
est31 <MTest31@outlook.com>
2018-12-10 01:20:37 +0100

    CLI parsing for the server as well
    We can set the listen addr for the server now

--- mehlon-server/main.rs
+++ mehlon-server/main.rs
@@ -1,12 +1,30 @@
 #![forbid(unsafe_code)]
 
 extern crate mehlon_server;
+extern crate structopt;
 
 use mehlon_server::{Server, ServerToClientMsg, ClientToServerMsg};
 use mehlon_server::generic_net::{TcpClientConn, TcpServerSocket, NetworkClientConn};
 
+use structopt::StructOpt;
+
+/// Mehlon server
+#[derive(StructOpt, Debug)]
+#[structopt(name = "mehlon")]
+struct Options {
+	/// Set the listen address
+	#[structopt(long = "listen")]
+	listen_addr :Option<String>,
+}
+
 fn main() {
-	let server_socket = TcpServerSocket::new();
+	let options = Options::from_args();
+
+	let server_socket = if let Some(addr) = options.listen_addr {
+		TcpServerSocket::with_socket_addr(addr)
+	} else {
+		TcpServerSocket::new()
+	};
 	let mut server = Server::new(server_socket);
 	server.run_loop();
 }
