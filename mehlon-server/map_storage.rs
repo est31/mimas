e2c470b64ca029198ef95a87a771a8919985c127
est31 <MTest31@outlook.com>
2019-02-04 03:55:02 +0100

    Add queries

--- mehlon-server/map_storage.rs
+++ mehlon-server/map_storage.rs
@@ -1,5 +1,6 @@
-use rusqlite::{Connection, NO_PARAMS};
-use map::MapChunkData;
+use rusqlite::{Connection, NO_PARAMS, OptionalExtension};
+use map::{MapChunkData, CHUNKSIZE};
+use StrErr;
 use nalgebra::Vector3;
 
 pub struct SqliteStorageBackend {
@@ -33,19 +34,43 @@ impl SqliteStorageBackend {
 	}
 }
 
+fn serialize_mapchunk_data(data :&MapChunkData) -> Vec<u8> {
+	unimplemented!()
+}
+
+fn deserialize_mapchunk_data(data :&[u8]) -> Result<MapChunkData, StrErr> {
+	unimplemented!()
+}
+
 impl StorageBackend for SqliteStorageBackend {
 	fn store_chunk(&mut self, pos :Vector3<isize>,
-			data :&MapChunkData) {
-		// TODO
+			data :&MapChunkData) -> Result<(), StrErr> {
+		let pos = pos / CHUNKSIZE;
+		let data = serialize_mapchunk_data(&data);
+		// TODO prepare this statement
+		self.conn.execute_named("UPDATE OR INSERT INTO chunks (x, y, z, content) \
+			VALUES (:x, :y, :z, :content)",
+			&[(":x", &pos.x), (":y", &pos.y), (":z", &pos.z), (":content", &data)])?;
+		Ok(())
 	}
-	fn load_chunk(&mut self, pos :Vector3<isize>) -> MapChunkData {
-		// TODO
-		unimplemented!()
+	fn load_chunk(&mut self, pos :Vector3<isize>) -> Result<Option<MapChunkData>, StrErr> {
+		let pos = pos / CHUNKSIZE;
+		// TODO prepare this statement
+		let data :Option<Vec<u8>> = self.conn.query_row("SELECT content FROM chunks WHERE x=?,y=?,z=?",
+			&[&pos.x, &pos.y, &pos.z],
+			|row| row.get(0)
+		).optional()?;
+		if let Some(data) = data {
+			let chunk = deserialize_mapchunk_data(&data)?;
+			Ok(Some(chunk))
+		} else {
+			Ok(None)
+		}
 	}
 }
 
 pub trait StorageBackend {
 	fn store_chunk(&mut self, pos :Vector3<isize>,
-			data :&MapChunkData);
-	fn load_chunk(&mut self, pos :Vector3<isize>) -> MapChunkData;
+			data :&MapChunkData) -> Result<(), StrErr>;
+	fn load_chunk(&mut self, pos :Vector3<isize>) -> Result<Option<MapChunkData>, StrErr>;
 }
