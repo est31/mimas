e5743860d34ec6cf3539705106de8878b9513817
est31 <MTest31@outlook.com>
2019-08-01 06:19:47 +0200

    Try different offsets in recipe matching

--- mehlon-server/crafting.rs
+++ mehlon-server/crafting.rs
@@ -9,16 +9,43 @@ pub struct Recipe {
 impl Recipe {
 	fn matches(&self,
 			inv :&SelectableInventory) -> bool {
-		if inv.stacks().len() != self.inputs.len() {
+		if inv.stacks().len() < self.inputs.len() {
 			return false;
 		}
-		inv.stacks().iter()
-			.zip(self.inputs.iter())
-			.all(|(other, ours)| {
-				let otc = other.content()
-					.map(|(m, _c)| m);
-				otc == *ours
-			})
+		// TODO we recompute this sqrt here every time.
+		// might be smarter to cache it?
+		let inv_size_sqrt = (inv.stacks().len() as f32).sqrt() as usize;
+		let recipe_size_sqrt = (self.inputs.len() as f32).sqrt() as usize;
+		let size_sqrt_diff = inv_size_sqrt - recipe_size_sqrt;
+		// Try all possible offsets
+		for offs_line in 0 ..= size_sqrt_diff {
+			for offs_col in 0 ..= size_sqrt_diff {
+				let matches = inv.stacks().iter()
+					.enumerate()
+					.all(|(i, stack)| {
+						let stc = stack.content().map(|(m, _c)| m);
+						let line = i / inv_size_sqrt;
+						let col = i % inv_size_sqrt;
+						let line_recipe = line.checked_sub(offs_line);
+						let col_recipe = col.checked_sub(offs_col);
+						if let (Some(line_recipe), Some(col_recipe)) = (line_recipe, col_recipe) {
+							if (line_recipe < recipe_size_sqrt) && (col_recipe < recipe_size_sqrt) {
+								let recipe_idx = line_recipe * recipe_size_sqrt + col_recipe;
+								return stc == self.inputs[recipe_idx];
+							}
+						}
+						// If we are outside the recipe, the inventory needs to be empty
+						stc == None
+					});
+				// If there is a match for this offset,
+				// return a match for the recipe
+				if matches {
+					return true;
+				}
+			}
+		}
+		// No offset found at which there was a match
+		return false;
 	}
 }
 
@@ -26,14 +53,6 @@ static RECIPES :&[Recipe] = &[
 	Recipe {
 		inputs : &[
 			Some(MapBlock::Tree),
-			None,
-			None,
-			None,
-			None,
-			None,
-			None,
-			None,
-			None,
 		],
 		output : (MapBlock::Wood, 4),
 	},
