821856696b5af2d8f5fc32a24620e01110f1d0f7
est31 <MTest31@outlook.com>
2019-08-02 04:46:18 +0200

    Put invs into separate variable

--- mehlon/ui.rs
+++ mehlon/ui.rs
@@ -354,16 +354,16 @@ impl InventoryMenu {
 
 		// TODO this is hacky, we change state in RENDERING code!!
 		if let Some((from_pos, to_pos, button)) = swap_command {
+			let invs = &mut [&mut self.craft_inv, &mut craft_output_inv,
+				&mut self.inv];
 			if button == MouseButton::Left {
 				SelectableInventory::merge_or_swap(
-					&mut[&mut self.craft_inv, &mut craft_output_inv,
-						&mut self.inv],
+					invs,
 					from_pos, to_pos);
 			}
 			if button == MouseButton::Right {
 				SelectableInventory::move_n_if_possible(
-					&mut[&mut self.craft_inv, &mut craft_output_inv,
-						&mut self.inv],
+					invs,
 					from_pos, to_pos, 1);
 			}
 		}
