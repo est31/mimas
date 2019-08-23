aecf80ebb25c484b363aa751aea21ce90a37b61e
est31 <MTest31@outlook.com>
2019-08-23 16:27:53 +0200

    Put lambert shading into fragment shader

--- mehlon/fragment-shader.glsl
+++ mehlon/fragment-shader.glsl
@@ -1,4 +1,5 @@
 #version 140
+in float vlamb;
 in vec4 vcolor;
 in vec4 vposition;
 
@@ -9,6 +10,9 @@ uniform vec2 fog_near_far;
 const vec4 fog = vec4(0.5, 0.5, 0.5, 1.0);
 
 void main() {
+	vec4 color_lamb = vlamb * vcolor;
+	color_lamb.a = vcolor.a;
+
 	float fog_factor = clamp((length(vposition) - fog_near_far.y) / fog_near_far.x, 0.0, 1.0);
-	fcolor = mix(vcolor, fog, fog_factor);
+	fcolor = mix(color_lamb, fog, fog_factor);
 }
--- mehlon/vertex-shader.glsl
+++ mehlon/vertex-shader.glsl
@@ -3,6 +3,7 @@ in vec3 position;
 in vec4 color;
 in vec3 normal;
 
+out float vlamb;
 out vec4 vcolor;
 out vec4 vposition;
 
@@ -18,8 +19,7 @@ void main() {
 	vec3 nnormal = normalize(normal);
 
 	// Lambertian shading
-	float fac = max(dot(dir_light_a, nnormal), 0.2) +
+	vlamb = max(dot(dir_light_a, nnormal), 0.2) +
 		max(dot(dir_light_b, nnormal), 0.2);
-	vcolor = fac * color;
-	vcolor.a = color.a;
+	vcolor = color;
 }
