b5451211bf695968777d2fc3654a9f88e5eb94ec
est31 <MTest31@outlook.com>
2020-09-12 04:21:24 +0200

    Halve the speed of the default hand
    The new speed is more reasonable for survival uses.

--- mimas-server/game_params.rs
+++ mimas-server/game_params.rs
@@ -160,7 +160,7 @@ fn default_hand_tool_groups(dig_group_ids :&mut NameIdMap<DigGroup>) -> Vec<Tool
 	vec![
 		ToolGroup {
 			group : dig_group_ids.get_or_extend("group:any"),
-			speed : 5.0,
+			speed : 2.5,
 			hardness : 1,
 		},
 	]
