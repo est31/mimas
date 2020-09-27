est31/mimas

commit 00fe7d310c9f7d40eaac4396f841675dde48f489

Send movement commands containing craft inv
This means that craft inventory
is now properly being stored and loaded.

mimas/client.rs
@@ -31,7 +31,7 @@ use mimas_server::generic_net::NetworkClientConn;
 use mimas_server::local_auth::{PlayerPwHash, HashParams};
 use mimas_server::config::Config;
 use mimas_server::map_storage::{PlayerPosition, PlayerIdPair};
-use mimas_server::inventory::{SelectableInventory, InventoryPos};
+use mimas_server::inventory::{SelectableInventory, InventoryPos, InventoryLocation};
 use mimas_server::game_params::{GameParamsHdl, DigGroup, ToolGroup};
 
 use mimas_meshgen::{Vertex, mesh_for_chunk, push_block,
@@ -112,27 +112,29 @@ pub struct Game<C :NetworkClientConn> {
 
 macro_rules! maybe_inventory_change {
 	($m:ident, $this:ident, $command:expr) => {
-		let mut command_sent = false;
 		if let Some(cmd) = $command {
 			// Needed because it complains about missing type annotations otherwise
 			let cmd :SwapCommand = cmd;
 
-			if cmd.from_pos.0 == 1 && cmd.to_pos.0 == 1 {
-				let from = InventoryPos {
-					stack_pos : cmd.from_pos.1,
-					location : None,
-				};
-				let to = InventoryPos {
-					stack_pos : cmd.to_pos.1,
-					location : None,
-				};
-				let msg = ClientToServerMsg::InventorySwap(from, to, cmd.only_move);
-				let _ = $this.srv_conn.send(msg);
-
-				command_sent = true;
+			fn ind_to_loc(ind :usize) -> InventoryLocation {
+				match ind {
+					0 => InventoryLocation::CraftInv,
+					1 => todo!(), // TODO something about craft output inv
+					2 => InventoryLocation::PlayerInv,
+					_ => unreachable!(),
+				}
 			}
-		}
-		if !command_sent && $m.inventory() != &$this.sel_inventory {
+			let from = InventoryPos {
+				stack_pos : cmd.from_pos.1,
+				location : ind_to_loc(cmd.from_pos.0),
+			};
+			let to = InventoryPos {
+				stack_pos : cmd.to_pos.1,
+				location : ind_to_loc(cmd.to_pos.0),
+			};
+			let msg = ClientToServerMsg::InventorySwap(from, to, cmd.only_move);
+			let _ = $this.srv_conn.send(msg);
+		} else if $m.inventory() != &$this.sel_inventory {
 			$this.sel_inventory = $m.inventory().clone();
 			let msg = ClientToServerMsg::SetInventory($this.sel_inventory.clone());
 			let _ = $this.srv_conn.send(msg);
@@ -153,23 +155,21 @@ macro_rules! maybe_chest_inventory_change {
 			// Needed because it complains about missing type annotations otherwise
 			let cmd :SwapCommand = cmd;
 
-			let location_from = if cmd.from_pos.0 == 0 {
-				Some($m.chest_pos())
-			} else {
-				None
+			let ind_to_loc = |ind :usize| -> InventoryLocation {
+				match ind {
+					0 => InventoryLocation::WorldMeta($m.chest_pos()),
+					1 => InventoryLocation::PlayerInv,
+					_ => unreachable!(),
+				}
 			};
+
 			let from = InventoryPos {
 				stack_pos : cmd.from_pos.1,
-				location : location_from,
-			};
-			let location_to = if cmd.to_pos.0 == 0 {
-				Some($m.chest_pos())
-			} else {
-				None
+				location : ind_to_loc(cmd.from_pos.0),
 			};
 			let to = InventoryPos {
 				stack_pos : cmd.to_pos.1,
-				location : location_to,
+				location : ind_to_loc(cmd.from_pos.0),
 			};
 			let msg = ClientToServerMsg::InventorySwap(from, to, cmd.only_move);
 			let _ = $this.srv_conn.send(msg);

mimas-server/inventory.rs
@@ -304,5 +304,18 @@ pub struct InventoryPos {
 	pub stack_pos :usize,
 	/// None if the local player's inventory.
 	/// Some(pos) if chest at given position.
-	pub location :Option<Vector3<isize>>,
+	pub location :InventoryLocation,
+}
+
+#[derive(Serialize, Deserialize, Clone, Copy, PartialEq, Eq)]
+pub enum InventoryLocation {
+	WorldMeta(Vector3<isize>),
+	PlayerInv,
+	CraftInv,
+}
+
+impl InventoryLocation {
+	pub fn is_world_meta(&self) -> bool {
+		matches!(self, InventoryLocation::WorldMeta(_))
+	}
 }

mimas-server/server.rs
@@ -3,7 +3,8 @@ use crate::config::Config;
 use crate::map::{self, Map, ServerMap, MapBackend, MapChunkData,
 	CHUNKSIZE, MapBlock, MetadataEntry};
 use crate::map_storage::{self, PlayerIdPair, PlayerPosition};
-use crate::inventory::{self, SelectableInventory, Stack, InventoryPos, InvRef};
+use crate::inventory::{self, SelectableInventory, Stack, InventoryPos,
+	InventoryLocation, InvRef};
 use crate::local_auth::{SqliteLocalAuth, AuthBackend, PlayerPwHash, HashParams};
 use crate::game_params::{GameParams, ServerGameParams, ServerGameParamsHdl};
 use anyhow::Result;
@@ -819,7 +820,7 @@ impl<S :NetworkServerSocket> Server<S> {
 		// Helper macro to save us some repetitive code
 		macro_rules! do_for_inv_ref {
 			($location:expr, $name:ident, $thing:expr) => {
-				if let Some(p) = $location {
+				if let InventoryLocation::WorldMeta(p) = $location {
 					// Move between two locations inside the chest
 					if let Some(mut hdl) = map_cell.borrow_mut().get_blk_meta_mut(p) {
 						if let Some(MetadataEntry::Inventory(inv)) = hdl.get().clone() {
@@ -835,7 +836,7 @@ impl<S :NetworkServerSocket> Server<S> {
 						// TODO log something about an attempted action in an unloaded chunk
 						return;
 					}
-				} else {
+				} else if InventoryLocation::PlayerInv == $location {
 					// Move inside the player's inventory
 
 					// Store the inventory inside a local variable so that
@@ -857,6 +858,28 @@ impl<S :NetworkServerSocket> Server<S> {
 					// TODO maybe send changed inventory to player?
 
 					invs.1
+				} else { // InventoryLocation::CraftInv
+					// Move inside the player's inventory
+
+					// Store the inventory inside a local variable so that
+					// the borrow to all players gets invalidated
+					let mut inv = {
+						let mut players = self.players.borrow_mut();
+						let player = &mut players.get_mut(&id).unwrap();
+						player.craft_inventory.clone()
+					};
+					let mut $name = &mut inv;
+
+					let invs = $thing;
+
+					{
+						let mut players = self.players.borrow_mut();
+						let player = &mut players.get_mut(&id).unwrap();
+						player.craft_inventory = invs.0.clone();
+					};
+					// TODO maybe send changed inventory to player?
+
+					invs.1
 				}
 			};
 		}
@@ -872,7 +895,7 @@ impl<S :NetworkServerSocket> Server<S> {
 				(inv, ())
 			});
 		} else {
-			if from_pos.location.is_some() && to_pos.location.is_some() {
+			if from_pos.location.is_world_meta() && to_pos.location.is_world_meta() {
 				// TODO log something about attempted move between two chests.
 				// For now, this is unsupported (and would panic anyways due to the RefCell).
 				return;

commit dd5e88d3acc0aa4f220fd4f9e07511ee7d22592e

Fix a regression caused by prior commit
Fixes simple mistake introduced by 00fe7d310c9f7d40eaac4396f841675dde48f489
breaking movement of items from/to chests

mimas/client.rs
@@ -172,7 +172,7 @@ macro_rules! maybe_chest_inventory_change {
 			};
 			let to = InventoryPos {
 				stack_pos : cmd.to_pos.1,
-				location : ind_to_loc(cmd.from_pos.0),
+				location : ind_to_loc(cmd.to_pos.0),
 			};
 			let msg = ClientToServerMsg::InventorySwap(from, to, cmd.only_move);
 			let _ = $this.srv_conn.send(msg);