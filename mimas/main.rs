a932d528ca0c68ed323a68f19e009fb6231cd302
est31 <MTest31@outlook.com>
2020-10-17 23:35:35 +0200

    Remove some unused dependencies from the mimas crate

--- mimas/main.rs
+++ mimas/main.rs
@@ -1,19 +1,8 @@
 #![forbid(unsafe_code)]
 
 extern crate anyhow;
-extern crate nalgebra;
-#[macro_use]
 extern crate glium;
-extern crate glium_glyph;
-extern crate num_traits;
-extern crate frustum_query;
-extern crate rand_pcg;
-extern crate rand;
 extern crate structopt;
-extern crate srp;
-extern crate sha2;
-extern crate image;
-extern crate dirs;
 
 extern crate mimas_client;
 extern crate mimas_server;
